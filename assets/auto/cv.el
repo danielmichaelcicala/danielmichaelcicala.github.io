af25f38c8698e2efe840ff5d7af26ffc06f0274e
danielmichaelcicala <cicala.daniel@gmail.com>
2019-02-05 11:20:36 -0800

    remove contact from cv

---
+++ assets/auto/cv.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "cv"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "left=0.75in" "top=0.6in" "right=0.75in" "bottom=0.6in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "Resume"
+    "Resume10"
+    "geometry"
+    "verbatim"
+    "multicol"))
+ :latex)
+
